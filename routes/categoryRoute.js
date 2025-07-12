7efd6f2f4642b992d94529dcb8169f8c8322832d
SaumyaSingh04 <saumya0419@gmail.com>
2025-07-12 11:49:53 +0530

    fix

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -13,9 +13,9 @@ const router = express.Router();
 
 router.post("/", upload.single("image"), createCategory);
 router.get("/", getCategories);
+router.get("/stats", getCategoryStats); // ✅ moved up
 router.get("/:id", getCategoryById);
 router.put("/:id", upload.single("image"), updateCategory);
-router.get("/stats", getCategoryStats);
 router.delete("/:id", deleteCategory);
 
 export default router;
